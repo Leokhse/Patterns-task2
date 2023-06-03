798c81d8c35660b1896cd08a2ce61513c5e6ff85
Leo <levan2312@yandex.ru>
2023-06-03 17:56:46 +0400

    initial commit

---
+++ src/test/java/ru/netology/testmode/data/UserDataGenerator.java
@@ -0,0 +1,63 @@
+package ru.netology.testmode.data;
+
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+
+import static io.restassured.RestAssured.given;
+
+public class UserDataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Gson gson = new Gson();
+
+    private UserDataGenerator() {
+    }
+
+    public static UserData generateUser(String status) {
+        String login = generateRandomLogin();
+        String password = generateRandomPassword();
+        UserData user = new UserData(login, password, status);
+        saveUser(user);
+        return user;
+    }
+
+    private static String generateRandomLogin() {
+        Faker faker = new Faker();
+        String login = faker.name().username();
+        return login;
+    }
+
+    private static String generateRandomPassword() {
+        Faker faker = new Faker();
+        String password = faker.internet().password();
+        return password;
+    }
+
+    private static void saveUser(UserData user) {
+        given()
+                .spec(requestSpec)
+                .body(gson.toJson(user))
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    @Value
+    public static class UserData {
+        String login;
+        String password;
+        String status;
+    }
+}
---
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -0,0 +1,33 @@
+package ru.netology.testmode.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.testmode.data.UserDataGenerator;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.baseUrl = "http://localhost:9999";
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        UserDataGenerator.UserData registeredUser = UserDataGenerator.generateUser("active");
+
+        open("/");
+        $(byName("login")).setValue(registeredUser.getLogin());
+        $(byName("password")).setValue(registeredUser.getPassword());
+        $(".button").click();
+        $("div#root").should(appear);
+        $(".heading_size_l").shouldHave(text("Личный кабинет"));
+        $(".icon_name_bank-2449").shouldBe(visible);
+    }
+}
