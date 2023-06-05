8f727037a26ad78a3c67be8158d676430ded85fb
Leo <levan2312@yandex.ru>
2023-06-06 00:43:36 +0400

    new test

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -29,4 +29,37 @@ class AuthTest {
         $(".heading_size_l").shouldHave(text("Личный кабинет"));
         $(".icon_name_bank-2449").shouldBe(visible);
     }
+    @Test
+    @DisplayName("Should fail login with incorrect password")
+    void shouldFailLoginWithIncorrectPassword() {
+        UserDataGenerator.UserData user = UserDataGenerator.generateUser("active");
+
+        open("/");
+        $(byName("login")).setValue(user.getLogin());
+        $(byName("password")).setValue("incorrect_password");
+        $(".button").click();
+        $(".notification__content").shouldHave(text("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should fail login with incorrect login")
+    void shouldFailLoginWithIncorrectLogin() {
+        UserDataGenerator.UserData user = UserDataGenerator.generateUser("active");
+
+        open("/");
+        $(byName("login")).setValue("incorrect_login");
+        $(byName("password")).setValue(user.getPassword());
+        $(".button").click();
+        $(".notification__content").shouldHave(text("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should fail login with unregistered user")
+    void shouldFailLoginWithUnregisteredUser() {
+        open("/");
+        $(byName("login")).setValue("unregistered_user");
+        $(byName("password")).setValue("password");
+        $(".button").click();
+        $(".notification__content").shouldHave(text("Ошибка! Неверно указан логин или пароль"));
+    }
 }
